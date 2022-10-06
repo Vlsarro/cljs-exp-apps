547a3f306f21a4582b1be0105e0ed057d50a204f
Vlsarro <drekmaro@gmail.com>
2022-10-06 15:34:59 +0300

    implement converter with state

--- temp-converter/src/temp_converter/core.cljs
+++ temp-converter/src/temp_converter/core.cljs
@@ -1,32 +1,58 @@
 (ns temp-converter.core
     (:require
-      [reagent.core :as r]
-      [reagent.dom :as d]))
+     [temp-converter.model]
+     [reagent.core :as r]
+     [reagent.dom :as d]))
+
+(defonce app-state (r/atom {:unit-from "f" :unit-to "c" :temperature "0"}))
 
 ;; -------------------------
 ;; Views
 
+(defn radio-option [value name]
+  (let [id (str name "-" value)
+        label (get temp-converter.model/temperature-unit-name-map value)]
+    [:div.radio-option
+     [:input {
+              :type "radio"
+              :name name
+              :value value
+              :id id
+              :checked (= value (get @app-state (keyword name)))
+              :on-change #(swap! app-state assoc (keyword name) (-> % .-target .-value))}]
+     [:label {:for id} label]]))
+
+(defn unit-control [label radio-name]
+  (let [temp-units (keys temp-converter.model/converter-map)]
+    [:div.unit-control
+     [:p label]
+     (for [tu temp-units]
+       ^{:key (str radio-name "-" tu)}
+       [radio-option tu radio-name])]))
+
 (defn user-input []
   [:div.user-input
-   [:div.unit-control
-    [:p "Convert from"] 
-    [:div.radio-option
-     [:input#unit-c {:type "radio" :name "unit" :value "c" :checked "checked"}]
-     [:label {:for "unit-c"} "Celcius"]]
-    [:div.radio-option
-     [:input#unit-f {:type "radio" :name "unit" :value "f"}]
-     [:label {:for "unit-f"} "Fahreheit"]]]])
+   [unit-control "Convert from" "unit-from"]
+   [unit-control "Convert to" "unit-to"]])
 
 (defn temp-control []
   [:div.temp-control
    [:label {:for "temp"} "Temperature"]
-   [:input {:type "number" :id "temp" :name "temp"}]])
+   [:input {:type "number"
+            :id "temp"
+            :name "temp"
+            :value (:temperature @app-state)
+            :on-change #(swap! app-state assoc :temperature (-> % .-target .-value))}]])
 
 (defn converted-output []
-  [:div.converted-output
-   [:h3 "Converted Value"
-    [:span#temp-out "-"]
-    [:span#unit-out "F"]]])
+  (let [s @app-state
+        unit-from (:unit-from s)
+        unit-to (:unit-to s)
+        temperature (:temperature s)]
+    [:div.converted-output
+     [:h3 "Converted Value"
+      [:span#temp-out (temp-converter.model/convert-temperature temperature unit-from unit-to)]
+      [:span#unit-out (.toUpperCase unit-to)]]]))
 
 (defn temp-converter-page []
   [:div 
--- temp-converter/src/temp_converter/model.cljs
+++ temp-converter/src/temp_converter/model.cljs
@@ -6,9 +6,18 @@
 (defn c->f [deg-c]
   (+ (* deg-c 1.8) 32))
 
+(def temperature-unit-name-map
+  {"f" "Fahrenheit"
+   "c" "Celcius"})
+
 (def converter-map 
   {"f" {"c" f->c}
    "c" {"f" c->f}})
 
 (defn convert-temperature [value from to]
-  (.toFixed ((get-in converter-map [from to]) (js/parseInt value)) 2))
+  (let [temp-num (js/parseInt value)]
+    (if (js/isNaN temp-num)
+      "-"
+      (if (= from to)
+        temp-num
+        (.toFixed ((get-in converter-map [from to]) temp-num) 2)))))
