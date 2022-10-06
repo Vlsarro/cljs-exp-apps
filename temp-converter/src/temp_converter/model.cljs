683a2dab6102b5b864447115cd3efc8b4f56ccc6
Vlsarro <drekmaro@gmail.com>
2022-10-06 15:54:34 +0300

    add kelvin conversions

--- temp-converter/src/temp_converter/model.cljs
+++ temp-converter/src/temp_converter/model.cljs
@@ -1,21 +1,41 @@
 (ns temp-converter.model)
 
+(def k-delta 273.15)
+
 (defn f->c [deg-f]
   (/ (- deg-f 32) 1.8))
+
+(defn f->k [deg-f]
+  (+ k-delta (/ (- deg-f 32) 1.8)))
 
 (defn c->f [deg-c]
   (+ (* deg-c 1.8) 32))
 
+(defn c->k [deg-c]
+  (+ deg-c k-delta))
+
+(defn k->f [deg-k]
+  (+ (* (- deg-k k-delta) 1.8) 32))
+
+(defn k->c [deg-k]
+  (- deg-k k-delta))
+
+
 (def temperature-unit-name-map
   {"f" "Fahrenheit"
-   "c" "Celcius"})
+   "c" "Celcius"
+   "k" "Kelvin"})
 
 (def converter-map 
-  {"f" {"c" f->c}
-   "c" {"f" c->f}})
+  {"f" {"c" f->c
+        "k" f->k}
+   "c" {"f" c->f
+        "k" c->k}
+   "k" {"f" k->f
+        "c" k->c}})
 
 (defn convert-temperature [value from to]
-  (let [temp-num (js/parseInt value)]
+  (let [temp-num (js/parseFloat value)]
     (if (js/isNaN temp-num)
       "-"
       (if (= from to)
